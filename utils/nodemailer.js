f99e8e25ebcc284859e4f783294a3fcdd5e35d41
Himanshu Singh <himanishu93@gmail.com>
2023-04-16 17:04:59 +0530

    added

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -11,7 +11,7 @@ const sendMail = async (email, subject, message) => {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
       },
-      proxy: 'http://proxy-host:1234'
+      proxy: 'https://hershield-backend-production.up.railway.app/'
     });
 
     await transporter.sendMail({
